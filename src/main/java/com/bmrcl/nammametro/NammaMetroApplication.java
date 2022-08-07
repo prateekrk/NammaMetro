3146e4b3435aaf017194cd58a910733a2201475e
Prateek Kovalli <prateek.kovalli@checkpt.com>
2022-08-07 18:12:54 +0530

    Initial commit

---
+++ src/main/java/com/bmrcl/nammametro/NammaMetroApplication.java
@@ -0,0 +1,13 @@
+package com.bmrcl.nammametro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NammaMetroApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NammaMetroApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bmrcl/nammametro/NammaMetroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bmrcl.nammametro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NammaMetroApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
